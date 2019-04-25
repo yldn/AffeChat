a4a81c9e60f08761c19b28d2f41e586657130989
yangliudn <ga92yin@mytum.de>
2019-04-25 08:30:22 +0200

    ServerFinished

---
+++ ChatRoomServer/ChatRoomServer/Client.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatRoomServer
+{
+    class Client
+    {
+        private Socket clientsocket;
+
+        public Client ( Socket s)
+        {
+             clientsocket = s;
+        }
+
+
+
+    }
+}
---
+++ ChatRoomServer/ChatRoomServer/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatRoomServer
+{
+    class Program
+    {
+        static List<Client> clientlist = new List<Client>();
+
+        static void Main(string[] args)
+        {
+            Socket tcpServer= new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+            tcpServer.Bind(new IPEndPoint(IPAddress.Parse("192.168.1.104"), 7788));
+            tcpServer.Listen(100);
+
+            Console.WriteLine("server running.");
+            while (true)
+            {
+                Socket clientSocket = tcpServer.Accept();
+                Console.WriteLine("a client is connected !");
+                //parse into CLient Obj
+                Client client = new Client(clientSocket);
+                //add to the list 
+                clientlist.Add(client);
+            }
+            
+
+
+            
+
+        }
+    }
+}
